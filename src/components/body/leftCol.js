11cfce21cadd297226b03c6fe00f57d661ef63c2
Drew Wobeck <dwobeck@gmail.com>
2018-07-10 21:58:22 +0000

    clean up filetree

--- src/components/body/leftCol.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class LeftCol extends Component { 
-    render() {
-        return(<div />)
-    }   
-}
-
-export default LeftCol;
--- src/components/body/rightCol.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class RightCol extends Component { 
-    render() {
-        return (<div />)
-    }   
-}
-
-export default RightCol;
